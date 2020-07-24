4491ab2840364ce0627eb978b064e3f08e0733a2
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-24 09:54:44 +0200

    add gitignore

--- NodeJS/connect.js
+++ NodeJS/connect.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Raharimalala12@',
+    password: '',
     database: 'socialnetdata'
 });
 
