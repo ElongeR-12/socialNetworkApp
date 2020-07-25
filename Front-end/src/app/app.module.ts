8316923d5554c1a003a03fa080ae78d0447f03d9
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-26 00:08:25 +0200

    import FormsModule to handle client forms

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { AdminComponent } from './admin/admin.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
