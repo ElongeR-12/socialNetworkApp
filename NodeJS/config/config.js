23f9991abffc25871609a0c52b395ec3424b501f
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-25 08:28:36 +0200

    updtate database server configuration

--- NodeJS/config/config.js
+++ NodeJS/config/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'secret': 'grokonez-super-secret-key',
+  'secret': 'mysecret-for-token',
   ROLEs: ['USER', 'ADMIN', 'PM']
 };
--- NodeJS/config/db.config.js
+++ NodeJS/config/db.config.js
@@ -1,3 +1,4 @@
+const env = require('./env.js');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
@@ -17,8 +18,8 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
  
-db.user = require('../model/user.model.js')(sequelize, Sequelize);
-db.role = require('../model/role.model.js')(sequelize, Sequelize);
+db.user = require('../models/user')(sequelize, Sequelize);
+db.role = require('../models/role')(sequelize, Sequelize);
  
 db.role.belongsToMany(db.user, { through: 'user_roles', foreignKey: 'roleId', otherKey: 'userId'});
 db.user.belongsToMany(db.role, { through: 'user_roles', foreignKey: 'userId', otherKey: 'roleId'});
