e2cd405adacb049f58283b268ffb7831b1295d1a
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-30 12:37:42 +0200

    update table relation, add post and like database relation

--- NodeJS/config/db.config.js
+++ NodeJS/config/db.config.js
@@ -1,5 +1,7 @@
 const env = require('./env.js');
 const Sequelize = require('sequelize');
+const { BelongsTo } = require('sequelize');
+const user = require('../models/user.js');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
   dialect: env.dialect,
@@ -17,11 +19,17 @@ const db = {};
  
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
- 
+
+db.blog = require('../models/blog')(sequelize, Sequelize);
 db.user = require('../models/user')(sequelize, Sequelize);
 db.role = require('../models/role')(sequelize, Sequelize);
+db.like = require('../models/like')(sequelize, Sequelize);
  
-db.role.belongsToMany(db.user, { through: 'user_roles', foreignKey: 'roleId', otherKey: 'userId'});
 db.user.belongsToMany(db.role, { through: 'user_roles', foreignKey: 'userId', otherKey: 'roleId'});
+db.user.hasMany(db.blog);
+db.blog.belongsTo(db.user);
+db.blog.hasMany(db.like);
+db.like.belongsTo(db.blog);
+
  
 module.exports = db;
