e0776959ed548adafc3075d05a82d023e03b1117
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-30 19:57:03 +0200

    implement like controllers to create text blog

--- NodeJS/controllers/like.js
+++ NodeJS/controllers/like.js
@@ -1,6 +1,14 @@
 const db = require('../config/db.config');
 const config = require('../config/config');
-
+const Like = db.like;
 exports.pushLike = (req, res) => {
-    console.log("on va liker")
+    Like.create({
+      blogId: req.body.blogId,
+      likeValue: req.body.likeValue,
+      likeUsername: req.body.likeUsername,
+      likeUserId: req.body.likeUserId
+    })
+      .then(like => {
+        res.json(like);
+      });
 }
