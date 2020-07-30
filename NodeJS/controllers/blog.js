1e499e02251c9ba82c9549851195b8d1e5d40ad8
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-07-30 12:42:25 +0200

    add blog & like controlles

---
+++ NodeJS/controllers/blog.js
@@ -0,0 +1,6 @@
+const db = require('../config/db.config');
+const config = require('../config/config');
+
+exports.create = (req, res) => {
+    console.log("on va créer")
+}
---
+++ NodeJS/controllers/like.js
@@ -0,0 +1,6 @@
+const db = require('../config/db.config');
+const config = require('../config/config');
+
+exports.pushLike = (req, res) => {
+    console.log("on va liker")
+}
