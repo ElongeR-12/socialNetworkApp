df1ed82026ac2b931676feebfd9f27f74f651dc5
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-08-14 10:36:08 +0200

    update blogCreation class model

--- Front-end/src/app/models/blog-creation.ts
+++ Front-end/src/app/models/blog-creation.ts
@@ -2,9 +2,14 @@ export class BlogCreation {
     userId: number;
     title: string;
     content: string;
-    constructor(userId: number, title: string, content: string) {
+    imageUrl: string;
+    postType: string
+    constructor(userId: number, title: string, content: string, imageUrl: string, postType: string) {
         this.userId = userId;
         this.title = title;
         this.content = content;
+        this.imageUrl = imageUrl;
+        this.postType = postType
     }
 }
+
