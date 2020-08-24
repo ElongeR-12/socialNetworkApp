fea49c2e227bf9dfb0052ef4b416ee341300678d
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-08-24 11:12:44 +0200

    implement upload post function

--- NodeJS/controllers/blog.js
+++ NodeJS/controllers/blog.js
@@ -2,42 +2,52 @@ const db = require('../config/db.config');
 const config = require('../config/config');
 const Blog = db.blog;
 exports.create = (req, res) => {
-  console.log(req.file);
-      const blogObject = req.body;
-        Blog.create(
-          // req.file ?
-          // {
+    console.log(req.file);
+    const blogObject = req.body;
+    Blog.create(
+            // req.file ?
+            // {
             {
-              userId: req.body.userId,
-            ...blogObject,
-            postType: req.body.postType
+                userId: req.body.userId,
+                ...blogObject,
+                likes: 0,
+                postType: req.body.postType
             }
-          //   postType: "IMAGE",
-          //   imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-          // } : 
-          // {
-          //   ...blogObject,
-          //   postType: "TEXT"
-          // }
+            //   postType: "IMAGE",
+            //   imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+            // } : 
+            // {
+            //   ...blogObject,
+            //   postType: "TEXT"
+            // }
         )
         .then(post => {
             res.send(post);
         })
         .catch(err => {
-          res.status(500).send({ reason: err.message });
+            res.status(500).send({ reason: err.message });
         })
+}
+exports.upload = (req, res) => {
+    res.send({
+        filename: req.file.filename
+    })
 }
 
 exports.getAllBlogs = (req, res) => {
-  Blog.findAll({order: [['updatedAt', 'DESC']]}).then(
-    (blogs) => {
-      res.status(200).json(blogs);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+    Blog.findAll({
+        order: [
+            ['updatedAt', 'DESC']
+        ]
+    }).then(
+        (blogs) => {
+            res.status(200).json(blogs);
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
 }
