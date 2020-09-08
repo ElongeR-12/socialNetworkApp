4cc1a5883684a9029c7f4025098413399c13ee67
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-09-08 17:00:42 +0200

    add likers aliase for user in it's blog manyTomany association

--- NodeJS/config/db.config.js
+++ NodeJS/config/db.config.js
@@ -24,27 +24,20 @@ db.user = require('../models/user')(sequelize, Sequelize);
 db.role = require('../models/role')(sequelize, Sequelize);
 db.like = require('../models/like')(sequelize, Sequelize);
  
-db.user.belongsToMany(db.role, { through: 'user_roles', foreignKey: 'userId', otherKey: 'roleId'});
-db.blog.belongsTo(db.user,{foreignKey: 'userId'});
+//create user and role association
+//through user_roles tables, an extra junction table 
+// extra table require two foreignKey
+db.role.belongsToMany(db.user, { through: 'user_roles', foreignKey: 'roleId', otherKey: 'userId'});
+db.user.belongsToMany(db.role, {through: 'user_roles', foreignKey: 'userId', otherKey: 'roleId'});
 
-db.user.hasOne(db.blog, {foreignKey: 'userId'});
-// db.user.hasMany(db.blog, {foreignKey: 'userId'});
-// db.user.hasMany(db.like, {foreignKey: 'userId'});
-// db.blog.belongsTo(db.user, {foreignKey: 'userId'});
-// db.blog.hasMany(db.like, {foreignKey: 'blogId'})
-// db.like.belongsTo(db.user, {foreignKey: 'userId', as: "liker"});
-// db.like.belongsTo(db.blog, {foreignKey: 'blogId'});
-
-db.user.belongsToMany(db.blog, {
-  through: db.like,
-  foreignKey: 'userId',
-  otherKey: 'blogId',
-});
-
-db.blog.belongsToMany(db.user, {
-  through: db.like,
-  foreignKey: 'blogId',
-  otherKey: 'userId',
-});
+// create blog association, add userId column to blog tables
+db.blog.belongsTo(db.user,{foreignKey: {
+  allowNull: false
+}});
+db.user.hasMany(db.blog);
+//create blog and user association
+//through like tables, an extra junction table 
+db.user.belongsToMany(db.blog, {as: 'likers', through: db.like, foreignKey: 'userId', otherKey: 'blogId'});
+db.blog.belongsToMany(db.user, {as: 'likers', through: db.like, foreignKey: 'blogId', otherKey: 'userId'});
 
 module.exports = db;
