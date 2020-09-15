27ac538f1b1eed995a7a6bfc26d53eeb5c99ea11
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-09-15 11:00:58 +0200

    implement blogs routes and blog controllers middleware to filter and paginate blogs content

--- NodeJS/controllers/blog.js
+++ NodeJS/controllers/blog.js
@@ -108,3 +108,41 @@ exports.getOneBlog = (req, res) => {
     });
 
   }
+
+  // Retrieve all Blogs from the database.
+exports.findAll = (req, res) => {
+  const { page, size, title } = req.query;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+  const { limit, offset } = getPagination(page, size);
+
+  Blog.findAndCountAll({ where: condition, limit, offset })
+    .then(data => {
+      const response = getPagingData(data, page, limit);
+      res.send(response);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
+  
+// find all published blog
+exports.findAllPublished = (req, res) => {
+  const { page, size } = req.query;
+  const { limit, offset } = getPagination(page, size);
+
+  Blog.findAndCountAll({ where: { postType: 'IMAGE' }, limit, offset })
+    .then(data => {
+      const response = getPagingData(data, page, limit);
+      res.send(response);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+};
--- NodeJS/routes/blog.js
+++ NodeJS/routes/blog.js
@@ -6,4 +6,8 @@ router.post('/post', multer, blogCtrl.create);
 router.get('/post', blogCtrl.getAllBlogs);
 router.get('/:id', blogCtrl.getOneBlog);
 router.get('/test/postAsso/:id', blogCtrl.testAsso);
+// Retrieve all Blogs
+router.get("/show/blogs", blogCtrl.findAll);
+// Retrieve all published Blogs
+router.get("/show/blogs/published", blogCtrl.findAllPublished);
 module.exports = router;
