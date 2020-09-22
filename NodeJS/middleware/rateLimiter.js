8afad8b59bdb197d3813ac431912219f08685dc0
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-09-23 00:34:13 +0200

    implement rate limiter middleware to limit login attempt with false password or id

---
+++ NodeJS/middleware/rateLimiter.js
@@ -0,0 +1,6 @@
+const rateLimit = require('express-rate-limit'); 
+exports.attemptLimit = rateLimit({
+    windowMs: 60 * 1000, // 1min in milliseconds
+    max: 2,
+    message: 'You have exceeded the 2 requests in one minut limit!', 
+});
