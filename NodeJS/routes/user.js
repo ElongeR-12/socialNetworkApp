7fde94ffd000c88e8093b402751ece19d590ec33
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-09-23 00:43:30 +0200

    implement joi and rate limiter in sinUp route and signin route respectively, require token to get one blog and to delete blog

--- NodeJS/routes/user.js
+++ NodeJS/routes/user.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const verifySignUp = require('../middleware/verifySignUp');
 const userCtrl = require('../controllers/user');
-
-router.post('/signup',[verifySignUp.checkDuplicateUserNameOrEmail], userCtrl.signup);
-router.post('/signin', userCtrl.signin);
-router.get('/:id', userCtrl.getOneUser);
-router.delete('/:id', userCtrl.deleteUser);
+const authJwt = require('../middleware/verifyJwtToken');
+const joiSchema = require('../middleware/joiSchema');
+const rateLimiter = require('../middleware/rateLimiter');
+router.post('/signup',[verifySignUp.checkDuplicateUserNameOrEmail], joiSchema.authenticateSchema, userCtrl.signup);
+router.post('/signin', rateLimiter.attemptLimit, userCtrl.signin);
+router.get('/:id', [authJwt.verifyToken], userCtrl.getOneUser);
+router.delete('/:id', [authJwt.verifyToken], userCtrl.deleteUser);
 // router.get('/users', userCtrl.getUsers);
 
 module.exports = router;
