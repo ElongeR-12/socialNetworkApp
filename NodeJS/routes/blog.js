22a31cea3a351eec3b27777a08c6c60f3c8e023e
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-09-23 00:05:56 +0200

    configure route to delete a blog, require token for all routes

--- NodeJS/routes/blog.js
+++ NodeJS/routes/blog.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const blogCtrl = require('../controllers/blog');
-const multer = require('../middleware/multer-config')
-router.post('/post', multer, blogCtrl.create);
-router.get('/post', blogCtrl.getAllBlogs);
-router.get('/:id', blogCtrl.getOneBlog);
-router.get('/test/postAsso/:id', blogCtrl.testAsso);
+const authJwt = require('../middleware/verifyJwtToken');
+const multer = require('../middleware/multer-config');
+router.post('/post', [authJwt.verifyToken], multer, blogCtrl.create);
+router.get('/post', [authJwt.verifyToken], blogCtrl.getAllBlogs);
+router.get('/:id', [authJwt.verifyToken], blogCtrl.getOneBlog);
+router.get('/test/postAsso/:id', [authJwt.verifyToken], blogCtrl.testAsso);
 // Retrieve all Blogs
-router.get("/show/blogs", blogCtrl.findAll);
-// Retrieve all published Blogs
-router.get("/show/blogs/published", blogCtrl.findAllPublished);
+router.get("/show/blogs", [authJwt.verifyToken], blogCtrl.findAll);
+router.delete('/delete/:id', [authJwt.verifyToken], blogCtrl.deleteBlog)
 module.exports = router;
