e92523a76c45dd18b0b13cc8acc3866f017da045
Elonge11 <47829151+Elonge11@users.noreply.github.com>
2020-09-22 23:54:09 +0200

    update blogservices by implementing getall function to retrieve all blogs paginated from server

--- Front-end/src/app/services/blogs.service.ts
+++ Front-end/src/app/services/blogs.service.ts
@@ -16,6 +16,8 @@ export class BlogsService {
   blogs$ = new Subject<BlogCreation[]>();
   private createBlogUrl = 'http://localhost:8080/api/create/post'; 
   private likeBlogUrl = 'http://localhost:8080/api/blogs/';
+  private baseUrl = 'http://localhost:8080/api/create/show/blogs';
+  private blogDeletionUrl = 'http://localhost:8080/api/create/delete/';
 
   constructor(private http: HttpClient) {}
 
@@ -65,4 +67,9 @@ export class BlogsService {
   dislikeBlog(id: number, likeInfo: LikeInfo) {
     return this.http.post<string>(this.likeBlogUrl + id + '/vote', likeInfo, httpOptions);
   }
+
+  getAll(params: any): Observable<any> {
+    return this.http.get(this.baseUrl, { params });
+  }
+  
 }
